a9a42cbbb4051b44295bc298188bc33e92179786
youravgjoe <droidboss47@gmail.com>
2014-12-27 12:49:04 -0700

    Added buttons, tried to make ball move left and right (not working yet).

--- Assets/Scripts/RedBrick.cs
+++ Assets/Scripts/RedBrick.cs
@@ -1,18 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class RedBrick : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-	
+public class RedBrick : MonoBehaviour 
+{
 	void OnCollisionEnter2D(Collision2D coll)
 	{
 	//make the brick disappear when the ball hits it.
--- Assets/Scripts/WhiteBrick.cs
+++ Assets/Scripts/WhiteBrick.cs
@@ -1,15 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class WhiteBrick : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+public class WhiteBrick : MonoBehaviour 
+{
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		//make the brick disappear when the ball hits it.
+		if(coll.gameObject.tag == "Ball")
+		{
+			gameObject.active = false;
+		}		
 	}
 }
