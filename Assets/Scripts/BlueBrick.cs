66ee6d93ddd3907177e505b50f6f7b28cc939dbc
Doug Cannon <douglascannon@ldschurch.org>
2015-01-02 14:59:13 -0700

    Added BlueBrick script

---
+++ Assets/Scripts/BlueBrick.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlueBrick : MonoBehaviour 
+{		
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		//make the brick disappear when the ball hits it.
+		if(coll.gameObject.tag == "Ball")
+		{
+			if (Bounce.ballColor == "Blue")
+			{	
+				Destroy(gameObject);
+				print("Hit!");
+//				--Bounce.brickCount;
+			}
+		}
+	}
+}
