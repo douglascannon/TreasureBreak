c6741bd1f0215fc5d183f8ad3a5a10477272be13
Doug Cannon <douglascannon@ldschurch.org>
2015-01-02 16:21:48 -0700

    Added wall brick

---
+++ Assets/Scripts/WallBrick.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallBrick : MonoBehaviour 
+{		
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		//make the brick disappear when the ball hits it.
+		if(coll.gameObject.tag == "Ball")
+		{
+			if (Bounce.ballColor == "Wall")
+			{	
+				Destroy(gameObject);
+				print("Hit!");
+//				--Bounce.brickCount;
+			}
+		}
+	}
+}
