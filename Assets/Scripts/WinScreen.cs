de277e9dae2ff71157fef9fa0f5e2366b80f6773
youravgjoe <droidboss47@gmail.com>
2015-01-06 00:02:42 -0700

    Assets push

---
+++ Assets/Scripts/WinScreen.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		//display text with time bonus, score, lives left, etc.
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.touchCount >= 1)
+		{
+			print ("touchCount >= 1");
+//			Application.LoadLevel("scene");
+		}
+	}
+	
+	public void ButtonPress()
+	{
+		Application.LoadLevel("level02");
+	}
+}
